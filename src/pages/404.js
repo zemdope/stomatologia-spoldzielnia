98a8d71c6e60cc2a922bccdcd39ef20b903a370e
Przemysław Michałowski <przemyslawmichalowski12@gmail.com>
2022-11-17 17:39:35 +0100

    add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <>
+      <h1>Page Not Found</h1>
+      <p>Oops, we couldn't find this page!</p>
+    </>
+  )
+}
