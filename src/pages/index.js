5a052146f8837383189db3f962a3664ff1606dd6
Przemysław Michałowski <przemyslawmichalowski12@gmail.com>
2022-11-21 14:19:03 +0100

    fix import path in indexPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ import {
 import { ContactForm } from '../components/ContactForm/ContactForm';
 import { AnimatePresence } from 'framer-motion';
 import SEO from '../components/SEO/SEO';
-import CookieConsent from 'react-cookie-consent';
+import { CookieConsent } from 'react-cookie-consent';
 
 export const Head = () => (
   <>
